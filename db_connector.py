d9ea6221b1a718e54a997cc639617bdbddb969ac
Malegr0 <schoening.andreas@gmx.de>
2021-06-10 17:49:44 +0200

    added doc's to each function

--- db_connector.py
+++ db_connector.py
@@ -1,3 +1,4 @@
+# TODO: Specify Docs of function, add Doc for File and variables
 import sys
 from mariadb import Error, connect
 
@@ -6,33 +7,62 @@ conn = None
 cur = None
 
 
-def get_all_synonyms():
-    cur.execute("SELECT synonym, generic_term FROM synonyms")
+# TODO: Add check for empty list, raise EmptyListError
+def get_all_synonyms() -> list[dict]:
+    """Return all synonyms
+
+    ADD DESCRIPTION
+
+    :return: Returns a list of dictionaries with all synonyms.
+    :raise EmptyListError: emptyList
+    """
+    cur.execute("SELECT synonym, id FROM synonyms")
     list = []
-    for synonym, generic_term in cur:
+    for synonym, synonym_id in cur:
         dictionary = {
             "synonym": synonym,
-            "generic_term": generic_term
+            "generic_term": synonym_id
         }
         list.append(dictionary)
-    print(list)
+    return list
 
 
-def get_generic_term(synonym):
+# TODO: add checks for wrong returns, raise Error
+def get_generic_term(synonym: str) -> str:
+    """Searchs for the generic term of a synonym.
+
+    ADD DESCRIPTION
+
+    :param synonym: String of the word for which the generic term should be found.
+    :return: Returns the generic term as string if there is one for the synonym.
+    :raise RASENError: Generic term is not in the database table.
+    """
     cur.execute("SELECT id FROM synonyms WHERE synonym=?", synonym)
     synonym_id = cur
     cur.execute("SELECT generic_terms FROM generic_terms WHERE id=?", synonym_id)
     generic_term = cur
-    # TODO: add checks for wrong returns
     return generic_term
 
 
-def get_answer(case_id):
+# TODO: add check for wrong case_id, raise InvalidCaseIDError
+def get_answer(case_id) -> str:
+    """Returns the answer for case_id
+
+    ADD DESCRIPTION
+
+    :param case_id: Integer of the specific answer.
+    :return: Returns the answer as string if there is one.
+    :raise InvalidCaseIDError: case_id is not in the database table.
+    """
     cur.execute("SELECT answer FROM matching_table WHERE caseID=?", case_id)
     return cur
 
 
 def init_db_connection():
+    """Initialize Database Connection
+
+    Initializes the connection to the database with spezific data.
+    """
     try:
         global conn
         conn = connect(
@@ -51,4 +81,6 @@ def init_db_connection():
 
 
 def close_db_connection():
+    """Close Database Connection
+    """
     conn.close()
