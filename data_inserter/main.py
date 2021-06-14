2daeba8fb7a48035c74bbfad36ed071c93d301a0
Malegr0 <schoening.andreas@gmx.de>
2021-06-14 21:45:06 +0200

    added files which will store teh data and methods to open and load the data

--- data_inserter/main.py
+++ data_inserter/main.py
@@ -1,3 +1,17 @@
+import json
+
+f = open("answers.txt",)
+answers_data = json.load(f)
+f.close()
+
+f = open("generic_terms.txt",)
+generics_terms_data = json.load(f)
+f.close()
+
+f = open("synonyms.txt",)
+synonyms_data = json.load(f)
+f.close()
+
 
 if __name__ == '__main__':
     print("Succefully executed!")
