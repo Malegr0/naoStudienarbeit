131c409aeece21c5d1d19aa7c47f47f0f651646d
Malegr0 <schoening.andreas@gmx.de>
2021-06-27 16:01:43 +0200

    added post endpoints to the server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request, jsonify
 
+import db_connector
+
 app = flask.Flask(__name__)
 # Change to False when using in production
 app.config["DEBUG"] = True
@@ -17,3 +19,25 @@ def get_db_answer(question):
 def get_request():
     question = request.form['question']
     return jsonify(get_db_answer(question))
+
+
+@app.route('/answers', methods=['POST'])
+def post_answer():
+    case_id = request.form.get('caseID')
+    keywords = request.form.get('keywords')
+    answer = request.form.get('answer')
+    db_connector.insert_answers(case_id, keywords, answer)
+
+
+@app.route('/genericTerms', methods=['POST'])
+def post_generic_term():
+    gn_id = request.form.get('id')
+    generic_term = request.form.get('generic_term')
+    db_connector.insert_generic_terms(gn_id, generic_term)
+
+
+@app.route('/synonyms', methods=['POST'])
+def post_synonyms():
+    synonym = request.form.get('synonym')
+    syn_id = request.form.get('id')
+    db_connector.insert_synonyms(synonym, syn_id)
