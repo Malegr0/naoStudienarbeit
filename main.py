069c6096aa3893067e864fb6f6282ab23d03ed8d
Malegr0 <schoening.andreas@gmx.de>
2021-10-13 10:26:03 +0200

    changed to pipenv as interpreter

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 
 import server
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #db_connector.init_db_connection()
     server.app.run(host='0.0.0.0', port=5050)
-    #db_connector.close_db_connection()
     print("Successfully executed!")
-
