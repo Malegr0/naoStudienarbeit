1b392a022f683ce152b97a78cf9ab2e4c06fc7e2
Malegr0 <schoening.andreas@gmx.de>
2021-11-07 14:26:38 +0100

    added config file and parser to read server config

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import server
+import argparse
+import configparser
+
+
+def evaluate_args():
+    parser = argparse.ArgumentParser(description="Help of arguments used by the serverside of nao!")
+    # Add arguments here
+    parser.add_argument("--r", action="store_true", help="Run the server!")
+    parser.add_argument("--i", action="store_true", help="Import template data into database")
+
+    # Check for argmunets and do stuff with it
+    args = parser.parse_args()
+    if args.i:
+        print("t1")
+    if args.r:
+        run_server()
+
+
+def run_server():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    server.app.run(host=config.get('server', 'host'), port=config.get('server', 'port'))
+
 
 if __name__ == '__main__':
-    server.app.run(host='0.0.0.0', port=5050)
-    print("Successfully executed!")
+    evaluate_args()
+    print("Skript done!")
