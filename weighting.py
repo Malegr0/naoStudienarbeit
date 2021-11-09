8e09a2cfa393cdf935eb3bbfc5c7ec8bd630bd2e
KaranIses <dominic.fietz@hotmail.de>
2021-11-09 14:32:20 +0100

    create weighting.py for calculating weight of keywords, some TODOs

---
+++ weighting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# TODO: Implement algorithm for calculating weight
+# TODO: Method with list as Input and list as Output
+# TODO: Save distinct keywords from input in another list
+# TODO: Output list as list of dictionaries (other output type)
