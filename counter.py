950ad0fc521cd18ed603db221498089bb7193a1c
KaranIses <dominic.fietz@hotmail.de>
2022-01-21 14:41:59 +0100

    added check for highest id and count

--- counter.py
+++ counter.py
@@ -37,18 +37,19 @@ def check_for_highest_id(question: list, counter: list) -> int:
     if counter is None:
         return None
     while i < len(counter):
+
         if highest_count is None:
             highest_count = counter[i].get("count")
             case_id = counter[i].get("case_id")
         elif highest_count < counter[i].get("count"):
             highest_count = counter[i].get("count")
             case_id = counter[i].get("case_id")
-            print("Aktuell höchstes Gewicht" + str(highest_count))
-            print("Aktuell höchste Case ID" + str(case_id))
         elif highest_count == counter[i].get("count"):
             t_case_id = check_for_higher_id(question, case_id, counter[i].get("case_id"))
             if t_case_id is not None:
                 case_id = t_case_id
+        print("Aktuell höchstes Gewicht" + str(highest_count))
+        print("Aktuell höchste Case ID" + str(case_id))
         i += 1
     return case_id
 
